f4ccca560c567e3dcd7cfd871745a940c0672069
Calvin Remsburg <cremsburg.dev@gmail.com>
2022-06-17 11:14:18 +0000

    update logging

--- files/python/firewall.py
+++ files/python/firewall.py
@@ -3,14 +3,30 @@
 
 # Standard library
 from typing import List, Optional
+import logging
 
 # Third Party
 from jnpr.junos import Device  # type: ignore
 from jnpr.junos.utils.config import Config  # type: ignore
-from jnpr.junos.exception import ConnectError, ConnectUnknownHostError, ConnectTimeoutError, ConnectRefusedError, ConnectAuthError
+from jnpr.junos.exception import (
+    ConnectError,
+    ConnectUnknownHostError,
+    ConnectTimeoutError,
+    ConnectRefusedError,
+    ConnectAuthError,
+)
 
 # Local
 from pydantic import BaseModel
+
+
+# Define logging level of our script
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+console_handler = logging.StreamHandler()
+log_format = "%(asctime)s | %(levelname)s: %(message)s"
+console_handler.setFormatter(logging.Formatter(log_format))
+logger.addHandler(console_handler)
 
 
 class Host(BaseModel):
@@ -57,28 +73,46 @@ class SrxHelper(BaseModel):
     inventory: List[Host]
     configuration: Configuration
 
+    def _connection_builder(self, each):
+        """Return a connection object."""
+
+        device_connection = Device(
+            host=each.ip,
+            user=self.credentials.username,
+            passwd=self.credentials.password,
+            ssh_private_key_file=self.credentials.sshkey,
+            gather_facts=False,
+        )
+
+        return device_connection
+
+    def _print_error(self, error):
+        """Return a message to the console regarding the error."""
+
+        return logger.error(error)
+
     def get_status(self):
         """Testing API reachability with a version pull."""
 
         try:
             """Build connection, print to screen hello world message."""
             for each in self.inventory:
-                dev = Device(
-                    host=each.ip,
-                    user=self.credentials.username,
-                    passwd=self.credentials.password,
-                    ssh_private_key_file=self.credentials.sshkey,
-                    gather_facts=False,
-                )
+                dev = self._connection_builder(each)
                 dev.open()
-
                 print(f"successfully tested connection to {each.name}")  # noqa T001
-
                 dev.close()
 
-        except (ConnectError, ConnectUnknownHostError, ConnectTimeoutError, ConnectRefusedError, ConnectAuthError) as response_error:
+        except (
+            ConnectError,
+            ConnectUnknownHostError,
+            ConnectTimeoutError,
+            ConnectRefusedError,
+            ConnectAuthError,
+        ) as response_error:
             """Gracefully exit upon reaching an error."""
-            print(f"unsuccessful connection to {each.name}: {response_error}")  # noqa T001
+
+            self._print_error(response_error)
+
             raise SystemExit from response_error
 
     def security_zones(self):
@@ -103,7 +137,7 @@ class SrxHelper(BaseModel):
                 configuration.load(
                     template_path="templates/security_zones.j2",
                     template_vars=self.configuration,
-                    format="set"
+                    format="set",
                 )
 
                 if configuration.pdiff():
@@ -116,7 +150,15 @@ class SrxHelper(BaseModel):
 
                 dev.close()
 
-        except (ConnectError, ConnectUnknownHostError, ConnectTimeoutError, ConnectRefusedError, ConnectAuthError) as response_error:
+        except (
+            ConnectError,
+            ConnectUnknownHostError,
+            ConnectTimeoutError,
+            ConnectRefusedError,
+            ConnectAuthError,
+        ) as response_error:
             """Gracefully exit upon reaching an error."""
-            print(f"unsuccessful connection to {each.name}: {response_error}")  # noqa T001
+
+            self._print_error(response_error)
+
             raise SystemExit from response_error
